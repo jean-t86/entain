77761d236989bc688845c4c6f73d36ce8a3843d1
Jean Tadebois <jpm.tadebois@gmail.com>
2023-06-25 21:29:53 +1000

    Refactor NedApi to use dependency injection.
    This is an important step as it will bring the codebase closer to being able to bring the Network layer under test. Unit Tests to begin with as per standard.

--- app/src/main/java/me/tadebois/entain/MainActivity.kt
+++ app/src/main/java/me/tadebois/entain/MainActivity.kt
@@ -10,9 +10,25 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import me.tadebois.entain.nedapi.ApiResponse
+import me.tadebois.entain.nedapi.NedApi
 import me.tadebois.entain.ui.theme.EntainTheme
+import timber.log.Timber
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var nedApi: NedApi
+
+    // Create a CoroutineScope to manage coroutines
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -26,6 +42,25 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+
+        coroutineScope.launch {
+            try {
+                val response = nedApi.getNextRaces()
+                // Process the response
+                handleApiResponse(response)
+            } catch (e: Exception) {
+                // Handle the error
+                handleError(e)
+            }
+        }
+    }
+
+    private fun handleError(e: Exception) {
+        Timber.w("Network Layer Under Construction.")
+    }
+
+    private fun handleApiResponse(response: ApiResponse) {
+        Timber.w("Network Layer Under Construction")
     }
 }
 
