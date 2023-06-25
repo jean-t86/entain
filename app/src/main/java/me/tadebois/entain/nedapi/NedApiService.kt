2953b9382a89ef7b07f68bbcf8157145f30cfc52
Jean Tadebois <jpm.tadebois@gmail.com>
2023-06-25 10:44:15 +1000

    Refactor Retrofit interface and create NedApi to call API endpoint.

--- app/src/main/java/me/tadebois/entain/nedapi/NedApiService.kt
+++ app/src/main/java/me/tadebois/entain/nedapi/NedApiService.kt
@@ -1,13 +1,12 @@
-package me.tadebois.entain
+package me.tadebois.entain.nedapi
 
-import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface NedApiService {
-    @GET("rest/v1/racing")
-    fun getNextRaces(
+    @GET("rest/v1/racing/")
+    suspend fun getNextRaces(
         @Query("method") method: String = "nextraces",
         @Query("count") count: Int = 2
-    ): Call<ApiResponse>
+    ): ApiResponse
 }
