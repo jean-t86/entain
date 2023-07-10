8c116c34ae05f3f4540480136f65421eaad1fa11
Jean Tadebois <jpm.tadebois@gmail.com>
2023-07-10 17:27:27 +1000

    Add UI mockup

--- app/src/main/java/me/tadebois/entain/MainScreen.kt
+++ app/src/main/java/me/tadebois/entain/MainScreen.kt
@@ -1,7 +1,11 @@
 package me.tadebois.entain
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -11,11 +15,13 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import me.tadebois.entain.ui.theme.EntainTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -43,11 +49,16 @@ fun EntainApp() {
         Surface(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(innerPadding)
-                .background(MaterialTheme.colorScheme.tertiaryContainer),
+                .padding(innerPadding),
             color = MaterialTheme.colorScheme.background
         ) {
-
+            Column(verticalArrangement = Arrangement.SpaceEvenly) {
+                NextRace()
+                NextRace()
+                NextRace()
+                NextRace()
+                NextRace()
+            }
         }
     }
 }
@@ -57,5 +68,36 @@ fun EntainApp() {
 fun EntainPreview() {
     EntainTheme {
         EntainApp()
+    }
+}
+
+@Composable
+fun NextRace(modifier: Modifier = Modifier.padding(16.dp)) {
+    val image = painterResource(id = R.drawable.racing_icon_horse)
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+    ) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier.height(60.dp)
+        )
+        Text(
+            text = "R4 Warrnambool".uppercase(), style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 16.dp, end = 16.dp)
+        )
+        Text(text = "1m 30s")
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = false)
+@Composable
+fun NextRacePreview() {
+    EntainTheme {
+        NextRace()
     }
 }
--- app/src/main/java/me/tadebois/entain/ui/theme/Type.kt
+++ app/src/main/java/me/tadebois/entain/ui/theme/Type.kt
@@ -11,8 +11,8 @@ import me.tadebois.entain.R
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontFamily = FontFamily(Font(R.font.open_sans)),
+        fontWeight = FontWeight.Bold,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
