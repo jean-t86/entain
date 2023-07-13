c26b2a5a29d4ac881b1bccf2b2ab0aa4a4ba61e1
Jean Tadebois <jpm.tadebois@gmail.com>
2023-07-13 15:07:12 +1000

    WIP Update MainActivity to make sure state of RaceViewModel.races works

--- app/src/main/java/me/tadebois/entain/MainActivity.kt
+++ app/src/main/java/me/tadebois/entain/MainActivity.kt
@@ -5,36 +5,55 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import me.tadebois.entain.db.Race
 import me.tadebois.entain.nedapi.ApiResponse
 import me.tadebois.entain.nedapi.NedApi
 import me.tadebois.entain.ui.theme.EntainTheme
 import timber.log.Timber
 import javax.inject.Inject
+import kotlin.random.Random
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    @Inject
-    lateinit var raceRepository: RaceRepository
-    private val raceViewModel: RaceViewModel by viewModels { RaceViewModelFactory(raceRepository) }
+    private val raceViewModel: RaceViewModel by viewModels()
 
     @Inject
     lateinit var nedApi: NedApi
 
-    // Create a CoroutineScope to manage coroutines
     private val coroutineScope = CoroutineScope(Dispatchers.Main)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        setContent {
+//            EntainTheme {
+//                EntainApp()
+//            }
+//        }
+
+//        addRace()
+
         setContent {
             EntainTheme {
-                EntainApp()
+                RaceListScreen()
             }
         }
+
+//        addRace()
 
         coroutineScope.launch {
             try {
@@ -48,6 +67,17 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    private fun addRace() {
+        raceViewModel.addRace(
+            Race(
+                "${Random.nextInt()}",
+                "R1",
+                "category_id",
+                System.currentTimeMillis()
+            )
+        )
+    }
+
     private fun handleError(e: Exception) {
         Toast.makeText(this, "Error", Toast.LENGTH_LONG).show()
         Timber.e(e)
@@ -56,6 +86,58 @@ class MainActivity : ComponentActivity() {
     private fun handleApiResponse(response: ApiResponse) {
         Toast.makeText(this, "Success", Toast.LENGTH_LONG).show()
     }
+
+    @Composable
+    fun RaceListScreen() {
+        val races = raceViewModel.races
+
+//        LaunchedEffect(races) {
+//            val test = races.toList()
+//            // Observe changes in races
+//            // Update your UI accordingly
+//            Timber.d("Watched: RaceListScreen: ${races.count()}")
+//        }
+
+        Scaffold(
+            content = { padding ->
+                Timber.d("Watched: RaceList call from RaceListScreen: ${races.count()}")
+                Column(Modifier.padding(padding), verticalArrangement = Arrangement.SpaceBetween) {
+                    RaceList(races)
+                    Bob()
+                }
+            }
+        )
+    }
+
+    @Composable
+    fun Bob() {
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "I do not get recompose")
+        }
+    }
+
+    @Composable
+    fun RaceList(races: SnapshotStateList<Race>) {
+        Button(onClick = {
+            addRace()
+//            raceViewModel.getAllRaces()
+        }) {
+            Text(text = "Test: ${races.count()}")
+        }
+        // Display the list of races
+        // You can use a LazyColumn, RecyclerView, or any other Composable to display the races
+        Timber.d("Watched: RaceList Composable: ${races.count()}")
+    }
+
+    @Preview
+    @Composable
+    fun previewRaceScreen() {
+        EntainTheme {
+            RaceListScreen()
+        }
+    }
 }
+
+
 
 
