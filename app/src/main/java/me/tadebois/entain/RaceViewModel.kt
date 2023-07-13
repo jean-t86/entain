a73b4f9e5c5ca51d1e1caebd79621ad29e19e000
Jean Tadebois <jpm.tadebois@gmail.com>
2023-07-13 15:02:48 +1000

    Update RaceViewModel to update _races backing property on Dao insert
    Also,
    - removed RaceViewModelFactory and instead delegate instantiation of ViewModel to Hilt.

--- app/src/main/java/me/tadebois/entain/RaceViewModel.kt
+++ app/src/main/java/me/tadebois/entain/RaceViewModel.kt
@@ -5,27 +5,31 @@ import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import me.tadebois.entain.db.Race
+import me.tadebois.entain.repository.RaceRepository
 import javax.inject.Inject
 
 @HiltViewModel
-class RaceViewModel @Inject constructor(private val raceRepository: RaceRepository) :
-    ViewModel() {
-    val races: SnapshotStateList<Race> = mutableStateListOf()
+class RaceViewModel @Inject constructor(private val raceRepository: RaceRepository) : ViewModel() {
+    private val _races = mutableStateListOf<Race>()
+    val races: SnapshotStateList<Race> = _races
 
-    fun addRace(race: Race) {
-        viewModelScope.launch(Dispatchers.IO) {
-            raceRepository.insertRace(race)
-        }
+    init {
+        getAllRaces()
     }
 
     fun getAllRaces() {
-        viewModelScope.launch(Dispatchers.IO) {
-            val allRaces = raceRepository.getAllRaces()
-            races.clear()
-            races.addAll(allRaces)
+        viewModelScope.launch {
+            _races.clear()
+            _races.addAll(raceRepository.getAllRaces())
+        }
+    }
+
+    fun addRace(race: Race) {
+        viewModelScope.launch {
+            _races.add(race)
+            raceRepository.insertRace(race)
         }
     }
 }
