be62847bc8099e92a9a8e3ac5094343026579cb9
Jean Tadebois <jpm.tadebois@gmail.com>
2023-07-17 13:02:04 +1000

    Refactor logic for syncing data from API to DB inside MainActivity

--- app/src/main/java/me/tadebois/entain/App.kt
+++ app/src/main/java/me/tadebois/entain/App.kt
@@ -2,10 +2,7 @@ package me.tadebois.entain
 
 import android.app.Application
 import androidx.work.Configuration
-import androidx.work.OneTimeWorkRequestBuilder
-import androidx.work.WorkManager
 import dagger.hilt.android.HiltAndroidApp
-import me.tadebois.entain.repository.DataSyncWorker
 import me.tadebois.entain.repository.DataSyncWorkerFactory
 import timber.log.Timber
 import javax.inject.Inject
@@ -19,17 +16,11 @@ class App : Application(), Configuration.Provider {
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
-        scheduleDataSync()
     }
 
     override fun getWorkManagerConfiguration(): Configuration {
         return Configuration.Builder()
             .setWorkerFactory(dataSyncWorkerFactory)
             .build()
-    }
-
-    private fun scheduleDataSync() {
-        val workRequest = OneTimeWorkRequestBuilder<DataSyncWorker>().build()
-        WorkManager.getInstance(applicationContext).enqueue(workRequest)
     }
 }
